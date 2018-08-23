ae959cba6373b5ff697658df8ba243e204a40086
HRAAON <15638269759@163.COM>
2018-08-23 20:09:07 +0800

    first提交

--- src/hfdtest1/class1.java
+++ src/hfdtest1/class1.java
@@ -12,6 +12,7 @@ public static void main(String[] args) {
 	int [] arr = {1,4,1,4,2,5,4,5,8,7,8,77,88,5,4,9,6,2,4,1,5};
 	Map< Integer, Integer> map = new HashMap<>();
 	
+	
 	for (int i : arr) {
 		Integer num = map.get(i);
 		//System.out.println(num);
